2cd1701167bd86338422bceaba72949c87680553
Jchin414 <96265286+Jchin414@users.noreply.github.com>
2022-01-27 15:26:09 -0800

    Update Problem3.py

--- Problem3.py
+++ Problem3.py
@@ -1,5 +1,6 @@
 #The prime factors of 13195 are 5, 7, 13 and 29.
 #What is the largest prime factor of the number 600851475143 
+# 1/17/22 9:15PM-10PM
 
 number = 600851475143 #number 
 i = 2 #sets the first prime number 
